d3d446bd7db99c9c4151fe25c9f4784d73251b05
tanyajha16 <55140264+tanyajha16@users.noreply.github.com>
2020-06-18 19:31:30 +0530

    added store

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import {configureStore} from './store';
+
+const store= configureStore();
+console.log('store',store.getState());
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
---
+++ src/reducers/posts.js
@@ -0,0 +1,5 @@
+export default function posts(state=[])
+{
+    return state;
+    // {posts: []}
+}
